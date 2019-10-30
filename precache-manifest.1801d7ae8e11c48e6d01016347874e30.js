482c6deb1707e16b5217b30611f0dbe968008495
Sasa Trifunovic <sasa.trifunovic@sbgenomics.com>
2019-10-30 15:33:30 +0100

    Updates

---
+++ precache-manifest.1801d7ae8e11c48e6d01016347874e30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52810398ed08fe631d3998bcfd269560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a3cef896dfb27dced3",
+    "url": "/static/css/main.259539ec.chunk.css"
+  },
+  {
+    "revision": "ab699c44a9072b91fec0",
+    "url": "/static/js/2.603ade2d.chunk.js"
+  },
+  {
+    "revision": "c2a3cef896dfb27dced3",
+    "url": "/static/js/main.405013fd.chunk.js"
+  },
+  {
+    "revision": "c0096f1e21a63e9144eb",
+    "url": "/static/js/runtime-main.ec275b3b.js"
+  }
+]);
